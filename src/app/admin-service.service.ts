407e53b794b8420e1daed2ee545d83a662303455
Pareekshith <pareekshith@pollmonk.co>
2024-01-29 13:07:54 +0530

    25th commit

--- src/app/admin-service.service.ts
+++ src/app/admin-service.service.ts
@@ -6,7 +6,7 @@ import { Observable, identity } from 'rxjs';
   providedIn: 'root'
 })
 export class AdminServiceService {
-  private baseUrl = 'chikku'; 
+  private baseUrl = 'chikkulive'; 
   authToken: any;
   
   constructor(private http: HttpClient) { }
